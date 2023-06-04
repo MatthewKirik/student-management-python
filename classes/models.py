95827b1181fd1ccf6af2cf245d34a5652eef68a7
Matthew Kirik <ikirik59@gmail.com>
2023-06-04 22:44:57 +0300

    Refactor and reformat

--- classes/models.py
+++ classes/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from users.models import User
 from courses.models import Course
 
+
 class Class(models.Model):
     class_id = models.AutoField(primary_key=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role': 'T'})
+    teacher = models.ForeignKey(
+        User, on_delete=models.CASCADE, limit_choices_to={'role': 'T'})
